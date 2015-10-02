37746fa892932634ba2f0b9ef26411e135bb5c49
m3libea <rociorommor@gmail.com>
2015-10-02 11:29:58 -0700

    Up to date

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,42 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "CS 232 Ruby on Rails Development"
+  end
+    
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end  
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end 
+
+  test "Should have contact on p or h1" do
+    get :contact
+    assert_select "p,h1", /(C||c)ontact/
+  end
+  
+  test "Should have page title" do
+    get :contact
+    assert_select "h1.page-title", {:count => 1}
+  end
 end
