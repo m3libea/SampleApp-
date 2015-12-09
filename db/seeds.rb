0016aa3193c9bc4d93634cc4fa45253c23046cb4
m3libea <rociorommor@gmail.com>
2015-12-08 21:13:35 -0800

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
+User.create!(name:  "Some Dude",
+             email: "dputnam@ccsf.edu",
+             password:              "t0pS3cret!",
+             password_confirmation: "t0pS3cret!",
+             activated: true,
+             activated_at: Time.zone.now)
+ 
+User.create!(name:  "NEW ADMIN USER",
+             email: "ADMIN_USER@SOMEDOMAIN.COM",
+             password:              "SECRET_PASSWORD",
+             password_confirmation: "SECRET_PASSWORD",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
@@ -23,3 +37,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
